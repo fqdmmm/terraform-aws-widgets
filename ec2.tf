4810985ce4a3fe8bdd5a393bb54355978e2208dd
Monica Miller <theorphanage@MacBook-Pro.local>
2023-10-17 11:19:40 -0700

    fixing ec2 for this terraform cloud thing

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-024e6efaf93d85776"
   instance_type = "t3.micro"
   subnet_id     = module.vpc.public_subnets[0]
 
